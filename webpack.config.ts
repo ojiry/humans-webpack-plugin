3e21059f1917cd7dd6ceaf0262c2694bd4a9ec3d
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-09-20 20:56:57 +0900

    Setup webpack

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  mode: "production",
+  entry: path.join(__dirname, "src", "index"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts?$/,
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+        ],
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+};
+
+export default config;
