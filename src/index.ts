8683c9be599accc788389e76bf02b2c30d3a3d3d
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-09-20 21:58:23 +0900

    Define HumansWebpackPlugin class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import * as webpack from "webpack";
+
+interface Options {
+  template: string;
+}
+
+class HumansWebpackPlugin {
+  options: Options;
+
+  constructor(options: Options) {
+    this.options = options
+  }
+
+  apply(compiler: webpack.Compiler) {
+
+  }
+}
+
+module.exports = HumansWebpackPlugin;
